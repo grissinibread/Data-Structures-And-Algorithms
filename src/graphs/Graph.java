08e46c89add724d190d6936d55102ea4223c5036
Alejandro Navarro <alexnavarro57@gmail.com>
2025-06-05 14:59:42 -0700

    Implemented depth first traversal

--- src/graphs/Graph.java
+++ src/graphs/Graph.java
@@ -110,4 +110,29 @@ public class Graph {
                     stack.push(neighbor);
         }
     }
+
+    public void traverseBreadthFirst(String root) {
+        var node = nodes.get(root);
+        if(node == null)
+            return;
+
+        Set<Node> visited = new HashSet<>();
+
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(node);
+
+        while(!queue.isEmpty()) {
+            var current = queue.remove();
+
+            if(visited.contains(current))
+                continue;
+
+            System.out.println(current);
+            visited.add(current);
+
+            for(var neighbor : adjacencyList.get(current))
+                if(!visited.contains(neighbor))
+                    queue.add(neighbor);
+        }
+    }
 }
