4262944f64524cd628a7917270ee71a8220f3262
Alejandro Navarro <94933917+grissinibread@users.noreply.github.com>
2025-06-05 18:35:28 -0700

    implemented selection sort

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import graphs.Graph;
 import graphs.WeightedGraph;
 import sortingAlgorithms.BubbleSort;
+import sortingAlgorithms.SelectionSort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         int[] numbers = { 7, 3, 1, 4, 6, 2, 3};
-        var sorter = new BubbleSort();
+        var sorter = new SelectionSort();
         sorter.sort(numbers);
 
         System.out.println(Arrays.toString(numbers));
---
+++ src/sortingAlgorithms/SelectionSort.java
@@ -0,0 +1,20 @@
+package sortingAlgorithms;
+
+public class SelectionSort {
+    public void sort(int[] array) {
+        for(var i = 0; i < array.length; i++) {
+            var minIndex = i;
+            for(var j = i; j < array.length; j++)
+                if(array[j] < array[minIndex])
+                    minIndex = j;
+
+            swap(array, i, minIndex);
+        }
+    }
+
+    private void swap(int[] array, int index1, int index2) {
+        var temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+}
